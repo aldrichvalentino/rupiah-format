95f3f1841dbc77c39b5f11940f179c182c2a4e46
Aldrich Halim <aldrich.halim@pinnacleinvestment.co.id>
2019-09-26 11:30:27 +0700

    feat: update ts definition file

--- index.d.ts
+++ index.d.ts
@@ -6,22 +6,30 @@ interface FormatOptions {
 }
 
 /**
-  * Function to format a number to Rupiah currency
+ * Function to format a number to Rupiah currency
  * @param number Amount of money in `integer`
  * @param options Formatter options
- * 
+ *
  * prefix: `string`, defaults to `Rp`
- * 
+ *
  * delimiter: `string`, defaults to `.`
- * 
+ *
  * useSuffix: `boolean`, if true, adds a suffix or
  * a custom suffix to the end of the string
- * 
+ *
  * customSuffix: `string`, defaults to `,00`
  */
-declare function index(
-  number: number,
-  options?: FormatOptions
-): string;
+declare function index(number: number, options?: FormatOptions): string;
 
-export = index;
+declare namespace index {
+  interface FormatFunction {
+    (number: number): string;
+  }
+
+  /**
+   * Function to create a formatter instance
+   * @param defaultOptions Format options
+   * @returns A formatter function with the given configs
+   */
+  function create(defaultOptions: FormatOptions): FormatFunction;
+}
